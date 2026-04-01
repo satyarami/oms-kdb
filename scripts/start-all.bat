39d2237f81862edc0224a418acbcca270f14301b
satya <satya.rami@gmail.com>
2026-03-31 20:54:17 -0400

    wip

--- scripts/start-all.bat
+++ scripts/start-all.bat
@@ -7,17 +7,17 @@ echo.
 
 REM Start Aeron Listener (Aeron -> Chronicle Queue)
 echo [1/3] Starting Aeron Listener...
-call start-aeron-listener.bat
+call "%~dp0start-aeron-listener.bat"
 timeout /t 3 /nobreak
 
 REM Start KDB Writer (Chronicle Queue -> kdb+)
 echo [2/3] Starting KDB Writer...
-call start-kdb-writer.bat
+call "%~dp0start-kdb-writer.bat"
 timeout /t 2 /nobreak
 
 REM Start Chronicle Queue Monitor (stdout)
 echo [3/3] Starting Chronicle Queue Monitor...
-call start-queue-monitor.bat
+call "%~dp0start-queue-monitor.bat"
 
 echo.
 echo All oms-kdb processes have been started in separate console windows.
